9016fde296ef0f077d8c1c5cf85681cc138c1f6d
MaxKondratiev <meandmywifeinfo@gmail.com>
2022-10-10 12:53:50 -0500

    RecyclerView.ViewHolder + Adapter + RecyclerView Problems with reUsing cells

--- app/src/main/java/com/example/kotlin1shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1shoppinglist/presentation/MainActivity.kt
@@ -7,48 +7,32 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin1shoppinglist.R
 import com.example.kotlin1shoppinglist.domain.ShopingItem
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var viewModel: MainViewModel
-    private   lateinit var  llshopList: LinearLayout
+    private lateinit var adapter: ShopListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        llshopList = findViewById(R.id.ll_shopList)
+        setupRecylcerView()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        
         viewModel.shopListLiveDataRe.observe(this) {
 
-            showDataList(it)
-
+            adapter.shopList = it
         }
 
     }
-    private fun showDataList(list: List<ShopingItem>) {
-        llshopList.removeAllViews()
-        for (item in list ) {
 
-            val layoutId = if(item.enabled) {
-                R.layout.item_shop_enabled
-            } else {
-                R.layout.item_shop_disabled
-            }
-//            val view = LayoutInflater.from(this).inflate(layoutId,llshopList,false)
-//            val tvName = view.findViewById<TextView>(R.id.tv_name)
-//            val tvCount = view.findViewById<TextView>(R.id.tv_count)
-//            tvName.text = item.name
-//            tvCount.text = item.count.toString()
-//            llshopList.addView(view)
-//            view.setOnLongClickListener {
-//                viewModel.changeEnableState(item)
-//                true
-//            }
-            
-        }
-
+    private fun setupRecylcerView() {
+        val recyclerViewShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        recyclerViewShopList.adapter = adapter
     }
+
 }
--- app/src/main/java/com/example/kotlin1shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/kotlin1shoppinglist/presentation/ShopListAdapter.kt
@@ -1,40 +1,64 @@
 package com.example.kotlin1shoppinglist.presentation
 
+import android.view.ContentInfo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin1shoppinglist.R
 import com.example.kotlin1shoppinglist.domain.ShopingItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopingItem>()
+    var shopList = listOf<ShopingItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    class ShopItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvName = view.findViewById<TextView>(R.id.tv_name)
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled,parent,
-            false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled, parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
 
-                 val shopItem = list[position]
-               holder.tvName.text  =  shopItem.name
-                holder.tvCount.text = shopItem.count.toString()
-        holder.itemView.setOnClickListener{
+        val shopItem = shopList[position]
+        val status = if ( shopItem.enabled) {
+            "Active"
+        }       else {
+            "Not Active"
+        }
+        holder.itemView.setOnClickListener {
             true
         }
+        if(shopItem.enabled) {
+            holder.tvName.text = "${shopItem.name}  $status"
+            holder.tvCount.text = shopItem.count.toString()
+            holder.tvName.setTextColor(ContextCompat.getColor(holder.itemView.context,
+            android.R.color.holo_red_dark))
+        }  else {
+            holder.tvName.text = ""
+            holder.tvCount.text = ""
+            holder.tvName.setTextColor(ContextCompat.getColor(holder.itemView.context,
+                android.R.color.white))
+        }
+
 
     }
 
     override fun getItemCount(): Int {
-       return list.size
+        return shopList.size
     }
 }
