08dc900d559631ff0474bd67bb7218313e6d2ec6
MaxKondratiev <meandmywifeinfo@gmail.com>
2022-10-15 12:42:56 -0500

    Maket

---
+++ app/src/main/java/com/example/kotlin1shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin1shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin1shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
