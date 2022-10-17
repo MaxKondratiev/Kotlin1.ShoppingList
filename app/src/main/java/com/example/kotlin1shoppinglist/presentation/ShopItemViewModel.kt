019f69f92a3b4db0931477c5e41381b8587f6a4d
MaxKondratiev <meandmywifeinfo@gmail.com>
2022-10-17 10:45:07 -0500

    Maket+ShopItemViewModel+LiveDatas+Validators

--- app/src/main/java/com/example/kotlin1shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/kotlin1shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.kotlin1shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.kotlin1shoppinglist.data.ShopItemRepositoryImpl
 import com.example.kotlin1shoppinglist.domain.AddItemUseCase
@@ -16,10 +18,34 @@ class ShopItemViewModel: ViewModel() {
     private val addShopItemUseCase = AddItemUseCase(repository)
     private val editItemUseCase = EditItemUseCase(repository)
 
+    //Errors  handlers
+    private val _errorInputName = MutableLiveData<Boolean>()
+            val errorInputName: LiveData<Boolean>
+                get() {
+                   return  _errorInputName
+                }
+    private  val _errorInputCount = MutableLiveData<Boolean>()
+             val  errorInputCount: LiveData<Boolean>
+             get() {
+                 return _errorInputCount
+             }
+    //  LiveData
+    private val _shopItem = MutableLiveData<ShopingItem>()
+            val shopItem: LiveData<ShopingItem>
+            get() {
+                return _shopItem
+            }
+    //   LiveData
+    private val _isTimeToFinishScreen = MutableLiveData<Unit>()
+            val  isTimeToFinishScreen : LiveData<Unit>
+                get() {
+                    return  _isTimeToFinishScreen
+                }
+
 
     fun getShopItem(shopingItemId: Int) {
         val item = getShopItemUseCase.getItem(shopingItemId)
-        
+        _shopItem.value = item
     }
 
     fun addShopItem( inputName: String? , inputCount: String?) {
@@ -28,6 +54,7 @@ class ShopItemViewModel: ViewModel() {
         if (validateInput(name, count)) {
             val item = ShopingItem(name, count,true)
             addShopItemUseCase.addItem(item)
+            _isTimeToFinishScreen.value = Unit
         }
     }
 
@@ -35,11 +62,12 @@ class ShopItemViewModel: ViewModel() {
         val name = validateName(inputName)
         val count = validateCount(inputCount)
         if (validateInput(name, count)) {
-            val item = ShopingItem(name, count,true)
-            editItemUseCase.editItem(item )
+           _shopItem.value?.let {
+               val item = it.copy(name,count)
+               editItemUseCase.editItem(item )
+               _isTimeToFinishScreen.value = Unit
+           }
         }
-
-
     }
 
     private fun validateName(name: String?): String{
@@ -58,13 +86,22 @@ class ShopItemViewModel: ViewModel() {
         var result = true
         if (name.isBlank()) {
             //TODO: Show error
+            _errorInputName.value = true
             result =   false
        }
         if (count <= 0 ) {
             //TODO: Show error
+            _errorInputCount.value = true
             result =   false
         }
         return  result
+    }
+
+    public fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+    public fun resetErrorInputCount() {
+        _errorInputCount.value = false
     }
 
 }
